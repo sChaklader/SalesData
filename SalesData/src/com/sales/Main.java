56c61c3b91a76d86a7c74fdbcdb0e27ef867309f
sChaklader <shchaklader@gmail.com>
2015-11-10 21:02:22 -0500

    My Sales Data

---
+++ SalesData/src/com/sales/Main.java
@@ -0,0 +1,20 @@
+package com.sales;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+
+	public static void displayGreeting(){
+		System.out.println("Hi my sale buddy");
+		System.out.println("Only sale data");
+	}
+	
+	
+	
+}
---
+++ SalesData/src/com/sales/SalesData.java
@@ -0,0 +1,14 @@
+package com.sales;
+
+public class SalesData {
+
+	int data[] = {0,4,42};
+
+public void display (){
+	
+	System.out.println("Implemented data");
+}
+
+	
+
+}
